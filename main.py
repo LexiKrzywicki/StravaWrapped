8c245818541971d20206497824376783f00d3a03
LexiKrzywicki <lexikrzywicki@yahoo.com>
2025-12-25 23:50:32 -0500

    Create one dictonary with all activities

--- main.py
+++ main.py
@@ -6,58 +6,33 @@ class StravaWrapped():
         self.year = year
         mask = self.data['Activity Date'].str.contains(self.year, case=False)
         self.df = self.data[mask]
-        self.activities = self.get_activties()
+        self.activities = sorted(self.get_activties())
 
     def get_activties(self) -> set:
         activities = set(self.df['Activity Type'])
         return activities
     
-    # Returns time in minutes
-    def total_time(self) -> dict:
-        total_times = []
-        for acitivty in self.activities:
-            matches = self.df['Activity Type'] == acitivty
-            total_time = float(self.df[matches]['Elapsed Time'].sum())
-            # TODO: Covert mins to min : sec
-            total_times.append(total_time/60)  # Convert to minutes
-        return dict(list(zip(self.activities, total_times)))
-    
-    # Returns distance in miles
-    def total_distance(self) -> dict:
-        total_distances = []
+    # TODO: add units?
+    def overview(self) -> dict:
+        overview = dict.fromkeys(self.activities, None)
         for activity in self.activities:
             matches = self.df['Activity Type'] == activity
-            total_distance = float(self.df[matches]['Distance'].sum())
-            total_distances.append(total_distance)
-        return dict(list(zip(self.activities, total_distances)))
-    
-    # Number times for each activity
-    def get_occurences(self):
-        counts = []
-        for acitivity in self.activities:
-            count = int(self.df['Activity Type'].value_counts()[acitivity])
-            counts.append(count)
-        
-        occurences = list(zip(self.activities, counts))
-        return dict(occurences)
-    
-    def get_furthest(self) -> dict:
-        furthests = []
-        for activity in self.activities:
-            matches = self.df['Activity Type'] == activity
+            total_distance = float(self.df[matches]['Distance'].sum())  # Need to convert from meters to feet?
             furthest = float(self.df[matches]['Distance'].max())
-            furthests.append(furthest)
-        return dict(list(zip(self.activities, furthests)))
+            # TODO: convert time to min : sec
+            elapsed_time = float(self.df[matches]['Elapsed Time'].sum()) / 60 
+            count = int(self.df['Activity Type'].value_counts()[activity])
+            overview[activity] = {"distance": total_distance, "furthest": furthest, "elapsed time": elapsed_time, "times": count}
+        return overview
+
 
 
 def main():
     df = pd.read_csv('activities.csv')
     wrapped_24 = StravaWrapped(df, "2024")
     wrapped_25 = StravaWrapped(df, "2025")
-    print(wrapped_24.get_occurences())
-    print(wrapped_24.total_distance())
-    print(wrapped_24.total_time())
-    print(wrapped_24.get_furthest())
+    print(wrapped_24.overview())
+    print(wrapped_25.overview())
 
 
 if __name__ == "__main__":
